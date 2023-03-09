b7c64a818b912bdc2b1d1df3f858020a88d5c296
Matt C <matthew.compton@mymail.champlain.edu>
2023-03-09 16:31:52 -0500

    cleanup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,7 +35,6 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          exclude: ['onions.md'],
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           //editUrl:
