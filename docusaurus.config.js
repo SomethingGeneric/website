9c0460af60f146ed6cfee5f8c83522cbe2495b2c
Matt C <matt@xhec.dev>
2023-09-29 02:27:45 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'siseor.jpg',
 
   // Set the production url of your site here
-  url: 'https://xhec.dev',
+  url: 'https://www.mattcompton.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
